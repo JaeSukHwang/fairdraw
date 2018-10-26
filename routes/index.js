26457c1277f6fff890ff77a359b256f5a1a93748
jaesuk <dajasin245@naver.com>
2018-10-27 01:56:06 +0900

    FirstRouting

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,21 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.get("/showstatus", function(req, res){
+	res.sendFile(__dirname + "/public/showstatus.html");
+})
+
+router.get("/enhancement", function(req, res){
+	res.sendFile(__dirname + "/public/enhancement.html");
+})
+
+router.get("/success", function(req, res){
+	res.sendFile(__dirname + "/public/success.html");
+})
+
+router.get("/showtransaction", function(req, res){
+	res.sendFile(__dirname + "/public/showtransaction.html");
+})
+
 
 module.exports = router;
