871a44ce232283a4e5e630efcb7635c1af1ea963
happysmileboy <t35114105@gmail.com>
2018-10-27 00:29:20 +0900

    express 설정

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var bodyParser  = require('body-parser');
+var mongoose    = require('mongoose');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -21,7 +23,12 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/blocknumber');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+
+var port = process.env.PORT || 8080;
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -37,6 +44,10 @@ app.use(function(err, req, res, next) {
   // render the error page
   res.status(err.status || 500);
   res.render('error');
+});
+
+var server = app.listen(port, function(){
+    console.log("Express server has started on port " + port)
 });
 
 module.exports = app;
