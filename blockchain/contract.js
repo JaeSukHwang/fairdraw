35bde4e5d9445979c17374185d87b024384fbd02
happysmileboy <t35114105@gmail.com>
2018-10-28 01:07:24 +0900

    klay api 사용

--- blockchain/contract.js
+++ blockchain/contract.js
@@ -6,31 +6,27 @@ const solc = require('solc');
 //
 // Contract.setProvider('http://localhost:8551');
 
-let helloPath = path.resolve(__dirname, 'contract', 'hello.sol');
-let source = fs.readFileSync(helloPath, 'utf8');
+let SolPath = path.resolve(__dirname, 'contract', 'fairdraw.sol');
+let source = fs.readFileSync(SolPath, 'utf8');
 let compiledContract = solc.compile(source, 1);
 
-let abi = JSON.parse(compiledContract.contracts[':Hello'].interface);
-let bytecode = compiledContract.contracts[':Hello'].bytecode;
+let abi = JSON.parse(compiledContract.contracts[':Fairdraw'].interface);
+let bytecode = compiledContract.contracts[':Fairdraw'].bytecode;
 
 let sampleaccounts = cav.klay.accounts.create();
 let gasEstimate = cav.klay.estimateGas({data: "0x" + bytecode})
 let myContract = new cav.klay.Contract(abi, {from : sampleaccounts.address, gas: gasEstimate});
 let contractAddress;
-const deploy = cav.klay.personal.unlockAccount("0x55A6F3603ffcEEdAa46bB545Bb28699559dc8460",'wnsgur12').then(
-    myContract.deploy({
+let tx_hash
+const unlockAccount = cav.klay.personal.unlockAccount("0x55A6F3603ffcEEdAa46bB545Bb28699559dc8460",'wnsgur12')
+
+const deploy = myContract.deploy({
         data: "0x" + bytecode
     }).send({
         from: "0x55A6F3603ffcEEdAa46bB545Bb28699559dc8460",
         gas: 1500000,
         gasPrice: '25000000000'
-    })
-        .on('receipt', function(receipt) {
-            contractAddress = receipt.contractAddress
-        })
-)
-
-const send = myContract.method.
+    }, function(error, transactionHash){})
 
 module.exports = {
     deploy : deploy
