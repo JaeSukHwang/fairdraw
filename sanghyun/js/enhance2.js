c7e065a74aa088c595e5ea905d137206034c2033
HSH4U2U <hsh700788@naver.com>
2018-10-27 23:48:09 +0900

    뽑기나 강화 후 나오는 화면 조금 빼고 완료

---
+++ sanghyun/js/enhance2.js
@@ -0,0 +1,24 @@
+var button = $('.button');
+var level = $('.level');
+var animation = $('.enhance');
+var blackBlock = $('.black_block');
+var result = $('.result');
+var resultLevel = $('#result_level');
+
+button.on('click', function () {
+    // 클릭 버튼 일시적으로 안 보이게
+    button.hide();
+    // 효과 나타나고 효과 서서히 빨라지게(아래 함수 참고)
+    level.addClass('enhance');
+
+    // 겉에 화면 검게 하기
+    setTimeout(function(){
+        blackBlock.fadeIn(2000);
+        level.fadeOut(2000);
+    }, 4000);
+    setTimeout(function(){
+        blackBlock.fadeOut(1000);
+        result.fadeIn(1000);
+        resultLevel.fadeIn(1000);
+    }, 6200);
+});
